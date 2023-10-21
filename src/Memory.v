ffbae94d5fbc67d6852a931dba6b22f12ef9842c
Adriel Chagay <adriel.chagay@mi.unc.edu.ar>
2023-10-20 23:11:32 -0300

    Update Memory.v

--- src/Memory.v
+++ src/Memory.v
@@ -1,40 +1,33 @@
 `timescale 1ns / 1ps
 
-// Para usar una memoria de 32bits y 64 lugares entonces se utilizara RAM #(32, 6) my_ram (...)
-// De esta forma tendría 2^6 lugares de memoria
+// Para usar una memoria de 32bits y 8 lugares entonces se utilizara RAM #(32, 8) my_ram (...)
 
-//module memory #(parameter DATA_WIDTH=8, ADDR_WIDTH=4) (
-module Memory #(parameter DATA_WIDTH=8, ADDR_WIDTH=4) (
+module tp3_memory #(parameter DATA_BUS=32, LENGTH=8) (
     input wire clk,
     input wire rst,
-    input wire [ADDR_WIDTH-1:0] address,
-    input wire write_enable,
-    input wire read_enable,
-    input wire [DATA_WIDTH-1:0] data_in,
-    output reg [DATA_WIDTH-1:0] data_out
+    input wire [$clog2(LENGTH)-1:0] i_addr,
+    input wire i_wr_en,
+    input wire [DATA_BUS-1:0] i_data,
+    output reg [DATA_BUS-1:0] o_data
 );
 
-reg [DATA_WIDTH-1:0] memory [0:2**ADDR_WIDTH-1]; // Parameterized memory size
 
-always @(posedge clk or posedge rst)
-begin
+// Celdas de 32 bits, y IF detecta ultimos 2 bits no son 00
+
+reg [DATA_BUS-1:0] mem [0:2**$clog2(LENGTH)-1];
+
+always @(posedge clk) begin
     if (rst) begin
         // Reset process
-        for ( integer i = 0; i < 2**ADDR_WIDTH; i = i + 1) begin
-            memory[i] <= 0;
-        end
+        for (integer i = 0; i < 2**$clog2(LENGTH); i = i + 1)
+            mem[i] <= 0;
+        o_data <= 0;
     end else begin
-        if (write_enable)
-            memory[address] <= data_in;
+        if (i_wr_en)
+            mem[i_addr] <= 0;
+        else
+            o_data <= mem[i_addr];
     end
-
-    data_out <= memory[address];
-end
-
-always @(posedge clk)
-begin
-    if (read_enable)
-        data_out <= memory[address];
 end
 
 endmodule
