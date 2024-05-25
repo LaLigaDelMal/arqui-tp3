1300315508172333da18e5628b8dcfda6c295fb3
Gaspar <gaspar.segura@mi.unc.edu.ar>
2024-05-25 19:45:53 -0300

    Added BEQ and BNE decoding logic

--- src/Instruction_Decoder.v
+++ src/Instruction_Decoder.v
@@ -8,10 +8,12 @@ module Instruction_Decode (
     output reg [15:0] o_imm,
     output reg [25:0] o_addr_offset,
     output reg o_flg_pc_modify,       // 1 if jump/branch, 0 if not
-    output reg o_flg_branch,          // 1 if branch, 0 if jump
-    output reg o_flg_addr_type,       // 0 if address comes from register, 1
+    output reg o_flg_link_ret,        // 1 if saves the return address, 0 if not
+    output reg [1:0] o_flg_addr_type, // 00 if address comes from register, 01 if the address is obtained by replacing the low 28 bits of the PC with the 26-bit offset, 10 if the address is obtained by adding the 16-bit offset to the base address shifted 2 bits
     output reg [4:0] o_link_reg,      // Link register for JAL and JALR
-    output reg [4:0] o_addr_reg       // Address register for JR and JALR
+    output reg [4:0] o_addr_reg,      // Address register for JR and JALR
+    output reg o_flg_cmp,             // 1 if compare, 0 if not
+    output reg o_flg_equal            // 1 if the compare checks if it's equal, 0 if not
 );
 
 reg [5:0] opcode;
@@ -32,10 +34,12 @@ always @(instruction) begin
                     o_imm = 16'b0;
                     o_addr_offset = 26'b0;
                     o_flg_pc_modify = 1'b1;
-                    o_flg_branch = 1'b0;
-                    o_flg_addr_type = 1'b0;
+                    o_flg_link_ret = 1'b0;
+                    o_flg_addr_type = 2'b00;
                     o_link_reg = 5'b0;
                     o_addr_reg = i_instr[25:21];
+                    o_flg_cmp = 1'b0;
+                    o_flg_equal = 1'b0;
                 end
                 6'b001001: begin            // JALR
                     o_funct = 6'b0;
@@ -46,10 +50,12 @@ always @(instruction) begin
                     o_imm = 16'b0;
                     o_addr_offset = 26'b0;
                     o_flg_pc_modify = 1'b1;
-                    o_flg_branch = 1'b1;
-                    o_flg_addr_type = 1'b0;
+                    o_flg_link_ret = 1'b1;
+                    o_flg_addr_type = 2'b00;
                     o_link_reg = i_instr[15:11];
                     o_addr_reg = i_instr[25:21];
+                    o_flg_cmp = 1'b0;
+                    o_flg_equal = 1'b0;
                 end
                 default: begin
                     o_funct = i_instr[5:0];
@@ -60,80 +66,122 @@ always @(instruction) begin
                     o_imm = 16'b0;
                     o_addr_offset = 26'b0;
                     o_flg_pc_modify = 1'b0;
-                    o_flg_branch = 1'b0;
+                    o_flg_link_ret = 1'b0;
+                    o_flg_addr_type = 2'b00;
+                    o_link_reg = 5'b0;
+                    o_addr_reg = 5'b0;
+                    o_flg_cmp = 1'b0;
+                    o_flg_equal = 1'b0;
+                end
+            endcase
+        6'b000010: begin                            // J
+            o_funct = 6'b0;
+            o_rs = 5'b0;
+            o_rt = 5'b0;
+            o_rd = 5'b0;
+            o_sa = 5'b0;
+            o_imm = 16'b0;
+            o_addr_offset = i_instr[25:0];
+            o_flg_pc_modify = 1'b1;
+            o_flg_link_ret = 1'b0;
+            o_flg_addr_type = 2'b01;
+            o_link_reg = 5'b0;
+            o_addr_reg = 5'b0;
+            o_flg_cmp = 1'b0;
+            o_flg_equal = 1'b0;
+        end
+        6'b000011: begin                            // JAL
+            o_funct = 6'b0;
+            o_rs = 5'b0;
+            o_rt = 5'b0;
+            o_rd = 5'b0;
+            o_sa = 5'b0;
+            o_imm = 16'b0;
+            o_addr_offset = i_instr[25:0];
+            o_flg_pc_modify = 1'b1;
+            o_flg_link_ret = 1'b1;
+            o_flg_addr_type = 2'b01;
+            o_link_reg = 5'b11111;    // GPR 31
+            o_addr_reg = 5'b0;
+            o_flg_cmp = 1'b0;
+            o_flg_equal = 1'b0;
+        end
+
+        default: begin
+            case (opcode[5:3])
+                3'b000: begin
+                    if (opcode[0] == 1'b0) begin    // BEQ
+                        o_funct = 6'b0;
+                        o_rs = i_instr[25:21];
+                        o_rt = i_instr[20:16];
+                        o_rd = 5'b0;
+                        o_sa = 5'b0;
+                        o_imm = 16'b0;
+                        o_addr_offset = {10'b0, i_instr[15:0]};
+                        o_flg_pc_modify = 1'b1;
+                        o_flg_link_ret = 1'b0;
+                        o_flg_addr_type = 2'b10;
+                        o_link_reg = 5'b0;
+                        o_addr_reg = 5'b0;
+                        o_flg_cmp = 1'b1;
+                        o_flg_equal = 1'b1;
+                    end else begin                  // BNE
+                        o_funct = 6'b0;
+                        o_rs = i_instr[25:21];
+                        o_rt = i_instr[20:16];
+                        o_rd = 5'b0;
+                        o_sa = 5'b0;
+                        o_imm = 16'b0;
+                        o_addr_offset = {10'b0, i_instr[15:0]};
+                        o_flg_pc_modify = 1'b1;
+                        o_flg_link_ret = 1'b0;
+                        o_flg_addr_type = 2'b10;
+                        o_link_reg = 5'b0;
+                        o_addr_reg = 5'b0;
+                        o_flg_cmp = 1'b1;
+                        o_flg_equal = 1'b0;
+                    end
+        // Chequear todas las flags y metodos de direccionamiento
+        // Below TBC
+                end
+                3'b010: begin
+                    o_rs = i_instr[25:21];
+                    o_rt = i_instr[20:16];
+                    o_rd = 5'b0;
+                    o_sa = 5'b0;
+                    o_imm = i_instr[15:0];
+                    o_addr_offset = 26'b0;
+                    o_flg_pc_modify = 1'b0;
+                    o_flg_link_ret = 1'b0;
                     o_flg_addr_type = 1'b0;
                     o_link_reg = 5'b0;
                     o_addr_reg = 5'b0;
                 end
-            endcase
-        endcase
-    end
-endmodule
-        // Below TBC
-        /*
-        6'b000010: begin
-            o_rs = 5'b0;
-            o_rt = 5'b0;
-            o_rd = 5'b0;
-            o_sa = 5'b0;
-            o_imm = 16'b0;
-            o_addr = i_instr[25:0];
+                3'b011: begin
+                    o_rs = i_instr[25:21];
+                    o_rt = i_instr[20:16];
+                    o_rd = 5'b0;
+                    o_sa = 5'b0;
+                    o_imm = i_instr[15:0];
+                    o_addr_offset = 26'b0;
+                    o_flg_pc_modify = 1'b0;
+                    o_flg_link_ret = 1'b0;
+                    o_flg_addr_type = 1'b0;
+                    o_link_reg = 5'b0;
+                    o_addr_reg = 5'b0;
+                end
+                default: begin
+                    o_rs = i_instr[25:21];
+                    o_rt = i_instr[20:16];
+                    o_rd = 5'b0;
+                    o_sa = 5'b0;
+                    o_imm = i_instr[15:0];
+                    o_addr_offset = 26'b0;
+                    o_flg_pc_modify = 1'b0;
+                    o_flg_link_ret = 1'b0;
+                    o_flg_addr_type = 1'b0;
+                    o_link_reg = 5'b0;
+                    o_addr_reg = 5'b0;
+                end
+            end
         end
-        6'b000011: begin
-            o_rs = 5'b0;
-            o_rt = 5'b0;
-            o_rd = 5'b0;
-            o_sa = 5'b0;
-            o_imm = 16'b0;
-            o_addr = i_instr[25:0];
-        end
-        default: begin
-            o_rs = i_instr[25:21];
-            o_rt = i_instr[20:16];
-            o_rd = 5'b0;
-            o_sa = 5'b0;
-            o_imm = i_instr[15:0];
-            o_addr = 26'b0;
-        end
-    endcase
-
-    if(o_opcode == 6'b000000) begin                                         // Can be R type or R with Jump -> Check funct
-
-        o_funct = i_instr[5:0];
-        if(o_function == 6'b001000 || o_function == 6'b001001) begin        // Is R/J type but with Jump ( JR or JALR )
-            o_rs        = i_instr[25:21];
-            o_rt        = 5'b0;
-            o_rd        = 5'b0;   // Corregir salida del registro (debe respetar el formato tipo R) 
-            o_shamt     = 5'b0;
-            o_imm       = 16'b0;
-            o_addr      = 26'b0;
-        end else begin                                                      // Is R type
-            o_rs        = i_instr[25:21];
-            o_rt        = i_instr[20:16];
-            o_rd        = i_instr[15:11];
-            o_shamt     = i_instr[10:6];
-            o_immediate = 16'b0;
-            o_addr      = 26'b0;
-        end
-
-    end else if(o_opcode == 6'b000010 || o_opcode == 6'b000011) begin       // Is J type( J or JAL )
-        o_rs        = 5'b0;
-        o_rt        = 5'b0;
-        o_rd        = 5'b0;
-        o_shamt     = 5'b0;
-        o_immediate = 16'b0;
-        o_addr      = i_instr[25:0];
-
-    end else begin                                                          // Is I type
-        o_rs        = i_instr[25:21];
-        o_rt        = i_instr[20:16];
-        o_rd        = 5'b0;
-        o_shamt     = 5'b0;
-        o_immediate = i_instr[15:0];                                        // Also used for branch offset
-        o_addr      = 26'b0;
-    end
-end
-
-                                //// Levantar flag de excepcion de instruccion no reconocida (ver ALU y AGU)
-endmodule
-*/
