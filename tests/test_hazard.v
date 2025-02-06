a5894a0f458e76fe1a19a5db9327e1b5a50f9ca4
Gaspar Segura <gaspar.segura@outlook.com>
2025-02-06 20:01:57 -0300

    Rename test module to test_hazard and update test cases for hazard detection; improve instruction memory setup

--- tests/test_hazard.v
+++ tests/test_hazard.v
@@ -1,33 +1,99 @@
 `timescale 1ns / 1ps
 
-module test_jump;
+module test_hazard;
 
 // Inputs
 reg clk;
-reg reset;
-reg inst_mem_wr_en;
-reg [31:0] inst_mem_addr;
-reg [31:0] inst_mem_data;
+reg rst;
 
 // Instantiate the module under test
 TOP_TOP dut(
     clk,
-    reset,
-    inst_mem_wr_en,
-    inst_mem_addr,
-    inst_mem_data
+    rst,
+    rx,
+    tx
 );
 
 
 ////////////////////////// Test program for the hazard unit //////////////////////////
-// This program tests the hazard unit by executing a series of instructions that
-// generate different types of hazards.
-// LUI $t3 0x00F0
-// J 0x30FFF0F
-// JR $t3
-// JAL 0x30FFF0F
-// JALR $t0 $t3
-// LUI $t0 0xFFFF
+
+// LOAD instruction that loads halfword SIGNED from 0x0 to $t0
+//////// LH $t0 0x0($t0)
+// Any instruction that uses $t0 as a source register
+//////// ADDI $t1 $t0 0x0004
+// JALR instruction that jumps to the address stored in $t1 and loads the return address to $t2
+//////// JALR $t2 $t1
+
+
+// Any instruction that modifies $t3
+////////// LUI $t3 0x00F0
+// Unconsequential instruction (does not modify anything meaningful)
+////////// NOP
+// JR instruction that jumps to the address stored in $t3
+////////// JR $t3
+
+
+// Any instruction that modifies $t4
+////////// LUI $t4 0xFFFF
+// Any instruction that modifies $t5
+////////// ADDI $t5 $t5 0x0004
+// BNE that compares $t4 and $t5 and jumps to a certain address if they are not equal
+////////// BNE $t4 $t5 0x0008
+// Any instruction that modifies $t6
+////////// LUI $t6 0x0001
+// NOP
+////////// NOP
+
+//******* Test if J or JAL introduces one stall in the pipeline to wait for the address to be selected in the PC (there should be also one delay slot).
+
+//// Write the first address of the data memory
+initial begin
+    dut.MA.u_Data_Memory.memory[0] = 8'hAA;
+    dut.MA.u_Data_Memory.memory[1] = 8'hBB;
+    dut.MA.u_Data_Memory.memory[2] = 8'hCD;
+    dut.MA.u_Data_Memory.memory[3] = 8'hEF;
+end
+
+///// Write program to the instruction memory
+initial begin
+    // LH $t0 0x0 ($t0)    0x85080000
+    dut.IF.u_Instruction_Memory.memory[0] = 8'h85;
+    dut.IF.u_Instruction_Memory.memory[1] = 8'h08;
+    dut.IF.u_Instruction_Memory.memory[2] = 8'h00;
+    dut.IF.u_Instruction_Memory.memory[3] = 8'h00;
+
+    // ADDI $t1 $t0 0x0004    0x21090004
+    dut.IF.u_Instruction_Memory.memory[4] = 8'h21;
+    dut.IF.u_Instruction_Memory.memory[5] = 8'h09;
+    dut.IF.u_Instruction_Memory.memory[6] = 8'h00;
+    dut.IF.u_Instruction_Memory.memory[7] = 8'h04;
+
+    // JALR $t2 $t1    0x01205009
+    dut.IF.u_Instruction_Memory.memory[8] = 8'h01;
+    dut.IF.u_Instruction_Memory.memory[9] = 8'h20;
+    dut.IF.u_Instruction_Memory.memory[10] = 8'h50;
+    dut.IF.u_Instruction_Memory.memory[11] = 8'h09;
+
+    //dut.IF.u_Instruction_Memory.memory[12] = 8'h8F;
+    //dut.IF.u_Instruction_Memory.memory[13] = 8'h0C;
+    //dut.IF.u_Instruction_Memory.memory[14] = 8'h00;
+    //dut.IF.u_Instruction_Memory.memory[15] = 8'h0C;
+
+    //dut.IF.u_Instruction_Memory.memory[16] = 8'h8F;
+    //dut.IF.u_Instruction_Memory.memory[17] = 8'h10;
+    //dut.IF.u_Instruction_Memory.memory[18] = 8'h00;
+    //dut.IF.u_Instruction_Memory.memory[19] = 8'h10;
+end
+
+initial begin
+    // Reset
+    rst = 1;
+    #100;
+    rst = 0;
+
+    dut.DU.state = 4'b0010;  // RUN state
+    //dut.DU.mips_clk_ctrl = 1;
+end
 
 
 initial begin
@@ -42,77 +108,7 @@ end
 
 
 initial begin
-
-    // LUI $t3 0x00F0
-    inst_mem_wr_en = 1;
-    inst_mem_addr = 0;
-    inst_mem_data = 32'h3C0B00F0;
-    #100;
-    inst_mem_wr_en = 0;
-    #100;
-
-    // J 0x30FFF0F
-    //inst_mem_wr_en = 1;
-    //inst_mem_addr = 4;
-    //inst_mem_data = 32'h0B0FFF0F;
-    //#100;
-    //inst_mem_wr_en = 0;
-    //#100;
-
-    // JR $t3
-    inst_mem_wr_en = 1;
-    inst_mem_addr = 4;
-    inst_mem_data = 32'h01600008;
-    #100;
-    inst_mem_wr_en = 0;
-    #100;
-
-    // JAL 0x30FFF0F
-    //inst_mem_wr_en = 1;
-    //inst_mem_addr = 4;
-    //inst_mem_data = 32'h0x0F0FFF0F;
-    //#100;
-    //inst_mem_wr_en = 0;
-    //#100;
-
-    // JALR $t0 $t3
-    //inst_mem_wr_en = 1;
-    //inst_mem_addr = 4;
-    //inst_mem_data = 32'h01604009;
-    //#100;
-    //inst_mem_wr_en = 0;
-    //#100;
-
-    // LUI $t0 0xFFFF
-    //inst_mem_wr_en = 1;
-    //inst_mem_addr = 8;
-    //inst_mem_data = 32'h3C08FFFF;
-    //#100;
-    //inst_mem_wr_en = 0;
-    //#100;
-
-    // ADDI $t1 $t1 0x0004
-    inst_mem_wr_en = 1;
-    inst_mem_addr = 8;
-    inst_mem_data = 32'h21290004;
-    #100;
-    inst_mem_wr_en = 0;
-    #100;
-
-    // ADDI $t1 $t1 0x0004
-    inst_mem_wr_en = 1;
-    inst_mem_addr = 12;
-    inst_mem_data = 32'h21290004;
-    #100;
-    inst_mem_wr_en = 0;
-    #100;
-
-    // Reset generation
-    reset = 1;
-    #100 reset = 0;
-
-    #2000;
-    reset = 0;
+    #3000;
 
     // End simulation
     $finish;
