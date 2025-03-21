6fa7e0ad1e3ece40cf682c15c84c9d3c91b2ed48
Adriex <adrielchagay@gmail.com>
2025-03-21 19:52:21 -0300

    Mejorar la interfaz de depuración: agregar manejo de errores, soporte multiplataforma para el puerto serie y mejorar la visualización de datos de depuración.

--- scripts/debug_interface.py
+++ scripts/debug_interface.py
@@ -1,6 +1,6 @@
 import serial
 from time import sleep
-
+import os
 #### Comandos que se pueden enviar
 LOAD=bytes([0b01101100])
 RUN=bytes([0b01110010])
@@ -14,6 +14,9 @@ def parse_debug_data(data: bytes):
     # Convert bytes to HEX
     data = data.hex()
     # Split HEX string into list of HEX values
+    if len(data) < REPORT_SIZE_BYTES:
+        print(f"Advertencia: Se esperaban {REPORT_SIZE_BYTES} bytes, pero se recibieron {len(data)} bytes. \n")
+    
     pc = data[0:8]
     cycles_count = data[8:16]
     registers = [data[i:i+8] for i in range(16, 272, 8)]
@@ -29,13 +32,19 @@ def read_program() -> bytes:
     with open(PROGRAM_FILE_PATH, "r") as file:
         data = file.read()
         data = data.replace("\n", "")
-    return data.encode()
+    return bytes.fromhex(data)
 
 
 class SerialInterface:
     def __init__(self):
+        port = None
+        if os.name == 'nt':
+            port = 'COM3'
+        else:
+            port = '/dev/ttyUSB0'
+            
         self.ser = serial.Serial(
-            port='/dev/ttyUSB0',
+            port=port,
             baudrate=9600,
             bytesize=serial.EIGHTBITS,
             parity=serial.PARITY_NONE,
@@ -62,6 +71,9 @@ if __name__ == "__main__":
     # Parse interactive commands from user
     while True:
         command = input("Enter command: ")
+        if command == "":
+            print("Command cannot be empty")
+            continue
         if command == "exit":
             break
         elif command == "l":
@@ -70,13 +82,22 @@ if __name__ == "__main__":
         elif command == "r":
             serial_port.send_data(RUN)
             data = serial_port.receive_data()
-            print(parse_debug_data(data))
+            
+            debug_data = parse_debug_data(data)
+            print("PC:", debug_data["pc"])
+            print("Cycles Count:", debug_data["cycles_count"])
+            print("Registers:")
+            for i, reg in enumerate(debug_data["registers"]):
+                print(f"  R{i}: {reg}")
+            print("Memory:")
+            for i, mem in enumerate(debug_data["memory"]):
+                print(f"  M{i}: {mem}")
         elif command == "s":
             serial_port.send_data(STEP)
             data = serial_port.receive_data()
             print(parse_debug_data(data))
         elif command == "n":
-            serial_port.send_data(STEP)
+            serial_port.send_data(NEXT)
             data = serial_port.receive_data()
             print(parse_debug_data(data))
         else:
